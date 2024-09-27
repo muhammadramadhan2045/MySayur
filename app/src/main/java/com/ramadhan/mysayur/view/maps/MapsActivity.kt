993039599c592b7bd77e622b0aa01ec1fedf8f37
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2024-09-27 23:39:56 +0700

    location tracker real time

--- app/src/main/java/com/ramadhan/mysayur/view/maps/MapsActivity.kt
+++ app/src/main/java/com/ramadhan/mysayur/view/maps/MapsActivity.kt
@@ -4,14 +4,26 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,151 +31,248 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Cap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.ramadhan.mysayur.R
 import com.ramadhan.mysayur.databinding.ActivityMapsBinding
+import java.util.concurrent.TimeUnit
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private var isTracking = false
 
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+    private var allLatLng = ArrayList<LatLng>()
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        mMap.uiSettings.isZoomControlsEnabled = true
-        mMap.uiSettings.isIndoorLevelPickerEnabled = true
-        mMap.uiSettings.isCompassEnabled = true
-        mMap.uiSettings.isMapToolbarEnabled = true
-        // Add a marker in Sydney and move the camera
-//        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-        val dicodingSpace = LatLng(-6.1753924, 106.8271528)
-        mMap.addMarker(
-            MarkerOptions().position(dicodingSpace).title("Dicoding Space")
-                .snippet("Menyediakan berbagai macam kursus online")
-        )
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(dicodingSpace, 17f))
-
-
-
-
-        mMap.setOnPoiClickListener { poi ->
-            mMap.addMarker(
-                MarkerOptions()
-                    .position(poi.latLng)
-                    .title(poi.name)
-                    .snippet(poi.placeId)
-            )?.showInfoWindow()
-
+        getMyLastLocation()
+        createLocationRequest()
+        createLocationCallback()
+        binding.btnStart.setOnClickListener {
+            if (!isTracking) {
+                clearMaps()
+                updateTrackingStatus(true)
+                startLocationUpdate()
+            } else {
+                updateTrackingStatus(false)
+                stopLocationUpdate()
+            }
         }
-
-        getMyLocation()
-        addManyMarker()
     }
 
-    private val boundsBuilder = LatLngBounds.Builder()
-    private fun addManyMarker() {
-        val tourismPlace = listOf(
-            TourismPlace("Eiffel Tower", 48.8584, 2.2945),
-            TourismPlace("Great Wall of China", 40.4319, 116.5704),
-            TourismPlace("Petra", 30.3285, 35.4444),
-            TourismPlace("Pyramid of Giza", 29.9792, 31.1342),
-            TourismPlace("Taj Mahal", 27.1751, 78.0421),
-            TourismPlace("Colosseum", 41.8902, 12.4922),
-            TourismPlace("Machu Picchu", -13.1631, -72.5450),
-            TourismPlace("Christ the Redeemer", -22.9519, -43.2106)
-        )
+    private fun stopLocationUpdate() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
 
-
-        tourismPlace.forEach { place ->
-            val latLng = LatLng(place.latitude, place.longitude)
-            val markerOptions = MarkerOptions().position(latLng).title(place.name)
-            mMap.addMarker(markerOptions)
-            boundsBuilder.include(latLng)
-        }
-
-        val bounds = boundsBuilder.build()
-        mMap.animateCamera(
-            CameraUpdateFactory.newLatLngBounds(
-                bounds,
-                resources.displayMetrics.widthPixels,
-                resources.displayMetrics.heightPixels,
-                300
+    private fun startLocationUpdate() {
+        try {
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
             )
-        )
+
+        } catch (e: SecurityException) {
+            Log.e("MapsActivity", "Security Exception: ${e.message}")
+        }
     }
 
+    private fun createLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    Log.d(
+                        "MapsActivity",
+                        "onLocationRes ${location.latitude}, ${location.longitude}"
+                    )
+
+                    //draw polyline
+                    allLatLng.add(LatLng(location.latitude, location.longitude))
+                    mMap.addPolyline(
+                        PolylineOptions()
+                            .color(Color.CYAN)
+                            .width(20f)
+                            .addAll(allLatLng)
+
+                    )
+
+
+                    //set boundaries
+                    boundsBuilder.include(LatLng(location.latitude, location.longitude))
+                    val bounds = boundsBuilder.build()
+                    mMap.animateCamera(
+                        CameraUpdateFactory.newLatLngBounds(
+                            bounds,
+                            64
+                        )
+                    )
+
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdate()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(isTracking){
+            startLocationUpdate()
+        }
+    }
+
+    private fun updateTrackingStatus(newStatus: Boolean) {
+        isTracking = newStatus
+        if (isTracking) {
+            binding.btnStart.text = "Stop Running"
+        } else {
+            binding.btnStart.text = "Start Running"
+        }
+    }
+
+
+    private val resolutionLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        when (result.resultCode) {
+            RESULT_OK -> {
+                Log.d("MapsActivity", "User has agreed to change location settings")
+            }
+
+            RESULT_CANCELED -> {
+                Log.d("MapsActivity", "User has not agreed to change location settings")
+            }
+        }
+    }
+
+    private fun createLocationRequest() {
+        val priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        val interval = TimeUnit.SECONDS.toMillis(1)
+        val maxWaitTime = TimeUnit.SECONDS.toMillis(1)
+
+        locationRequest = LocationRequest.Builder(
+            priority,
+            interval
+        ).apply {
+            setMaxUpdateDelayMillis(maxWaitTime)
+        }.build()
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client = LocationServices.getSettingsClient(this)
+        client.checkLocationSettings(builder.build())
+            .addOnSuccessListener {
+                getMyLastLocation()
+            }
+
+            .addOnFailureListener { e ->
+                if (e is ResolvableApiException) {
+                    try {
+                        resolutionLauncher.launch(
+                            IntentSenderRequest.Builder(e.resolution).build()
+                        )
+                    } catch (sendEx: Exception) {
+                        Toast.makeText(this, "${sendEx.message}", Toast.LENGTH_SHORT).show()
+                        Log.e("MapsActivity", "Error get location setting: ${sendEx.message}")
+                    }
+
+                }
+            }
+    }
+
+    private fun getMyLastLocation() {
+        if (checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) &&
+            checkPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    showStartMarker(location)
+                } else {
+                    Toast.makeText(this, "Lokasi tidak ditemukan", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        } else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun showStartMarker(location: Location) {
+        val startLocation = LatLng(location.latitude, location.longitude)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(startLocation)
+                .title("Lokasi awal saya")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 17f))
+    }
+
+    private var boundsBuilder = LatLngBounds.Builder()
 
     private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            getMyLocation()
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions[android.Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                getMyLastLocation()
+            }
+
+            permissions[android.Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                getMyLastLocation()
+            }
+
+            else -> {
+                // Permission denied
+            }
         }
     }
 
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
-    private fun getMyLocation() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            mMap.isMyLocationEnabled = true
-        } else {
-            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
-        }
+    private fun clearMaps(){
+        mMap.clear()
+        allLatLng.clear()
+        boundsBuilder = LatLngBounds.Builder()
     }
 
 
-    private fun vectorTBitmap(@DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
-        val vectorDrawable = ResourcesCompat.getDrawable(resources, id, null)
-            ?: return BitmapDescriptorFactory.defaultMarker()
-
-        val bitmap = Bitmap.createBitmap(
-            vectorDrawable.intrinsicWidth,
-            vectorDrawable.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
-        )
-
-        val canvas = Canvas(bitmap)
-        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
-        DrawableCompat.setTint(vectorDrawable, color)
-        vectorDrawable.draw(canvas)
-        return BitmapDescriptorFactory.fromBitmap(bitmap)
-    }
-
-
-    data class TourismPlace(
-        val name: String,
-        val latitude: Double,
-        val longitude: Double
-    )
 }
